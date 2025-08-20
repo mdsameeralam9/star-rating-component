34c63b3fc57ce3482d6c08d1c937b8106590c1d0
mdsameeralam9 <mdsameer.alam9@gmail.com>
2025-08-20 21:49:11 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import StarRating from "./components/StarRating";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen w-screen">
+      <StarRating />
+    </div>
+  );
+}
+
+export default App;
