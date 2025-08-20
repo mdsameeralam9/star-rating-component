544a4a0b57ef15efc95553b9f456fc4e94718e61
mdsameeralam9 <mdsameer.alam9@gmail.com>
2025-08-20 22:05:50 +0530

    render star icon

--- src/components/StarRating/index.tsx
+++ src/components/StarRating/index.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FaStar } from "react-icons/fa";
+import { FaRegStar } from "react-icons/fa";
 
-const StarRating = () => {
+interface StarRatingProps{
+  numberOfStar: number;
+}
+
+const StarRating:React.FC<StarRatingProps> = ({ numberOfStar=5 }) => {
   return (
-    <div>StarRating</div>
+    <div className='flex gap-2'>
+      {Array(numberOfStar).fill('').map((_, index) => (
+        <span className='cursor-pointer'><FaStar size={24} color="#f59e0b"/></span>
+      ))}
+    </div>
   )
 }
 
