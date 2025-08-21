3b975fa4f865f2099e5e2675f3eadfe5ac713ccc
mdsameeralam9 <mdsameer.alam9@gmail.com>
2025-08-21 23:46:10 +0530

    rating funtionality

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,6 @@ function App() {
   return (
     <div className="flex flex-col justify-center items-center h-screen w-screen gap-4">
       <StarRating onRatingChange={setCurrentRating} />
-      <p className="text-lg text-gray-700">
-        {currentRating > 0 ? `You rated: ${currentRating} star${currentRating > 1 ? 's' : ''}` : 'Click to rate'}
-      </p>
     </div>
   );
 }
